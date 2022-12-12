3e860ff4b51e41e2b3273ab48c94a2134b64f2cc
Kol <62248045+Kol-UI@users.noreply.github.com>
2022-12-12 14:05:20 +0100

    Add files via upload

---
+++ CompetitiveProgramming/2235. Add Two Integers/Solution.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace CompetitiveProgramming.AddTwoIntegers
+{
+    public class Solution
+    {
+        public static int Sum(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+    }
+}
+
---
+++ CompetitiveProgramming/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using CompetitiveProgramming.AddTwoIntegers;
+
+namespace CompetitiveProgramming
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            ClimbingStairsFunc();
+            AddTwoIntegersFunc();
+        }
+
+        public static void Space()
+        {
+            Console.WriteLine();
+        }
+
+        public static void Title(string StringTitle)
+        {
+            Space();
+            Console.WriteLine("--- {0} ---", StringTitle);
+        }
+
+        private static void ClimbingStairsFunc()
+        {
+            Space();
+            Title("70. Climbing Stairs");
+            // Climbing Stairs
+            // Case 1
+            int Case1_70 = ClimbingStairs.Solution.ClimbStairs(2);
+            Console.WriteLine(Case1_70);
+            // Case 2
+            int Case2_70 = ClimbingStairs.Solution.ClimbStairs(3);
+            Console.WriteLine(Case2_70);
+            // Case 3
+            int Case3_70 = ClimbingStairs.Solution.ClimbStairs(40);
+            Console.WriteLine(Case3_70);
+        }
+
+        private static void AddTwoIntegersFunc()
+        {
+            Space();
+            Title("2235. Add Two Integers");
+            // Add Two Intergers
+            // Case 1
+            int Case1_2235 = AddTwoIntegers.Solution.Sum(12, 5);
+            Console.WriteLine(Case1_2235);
+            // Case 2
+            int Case2_2235 = AddTwoIntegers.Solution.Sum(-10, 4);
+            Console.WriteLine(Case2_2235);
+        }
+    }
+}
