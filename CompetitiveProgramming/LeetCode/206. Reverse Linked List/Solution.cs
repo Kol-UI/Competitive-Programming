135f91fdf762b045df0915608f5c84280113ea17
Kol <62248045+Kol-UI@users.noreply.github.com>
2022-12-23 13:45:16 +0100

    Add files via upload

---
+++ CompetitiveProgramming/LeetCode/206. Reverse Linked List/Solution.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using static CompetitiveProgramming.LeetCode.AddTwoNumbers.Solution;
+
+namespace CompetitiveProgramming.LeetCode.ReverseLinkedList
+{
+    // Given the head of a singly linked list, reverse the list, and return the reversed list.
+    // Input: head = [1,2]
+    // Output: [2,1]
+
+    /**
+     * Definition for singly-linked list.
+     * public class ListNode {
+     *     public int val;
+     *     public ListNode next;
+     *     public ListNode(int val=0, ListNode next=null) {
+     *         this.val = val;
+     *         this.next = next;
+     *     }
+     * }
+     */
+
+
+    public class Solution
+    {
+        public static ListNode? ReverseList(ListNode head)
+        {
+            if (head is null)
+            {
+                return null;
+            }
+            ListNode tail = null, next;
+
+            while (head != null)
+            {
+                next = head.next;
+                head.next = tail;
+                tail = head;
+                head = next;
+            }
+
+            return tail;
+        }
+    }
+}
+
