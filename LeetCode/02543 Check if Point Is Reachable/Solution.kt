b3a3c34ca16129370ad9024780b00c5041053588
Kol <62248045+Kol-UI@users.noreply.github.com>
2023-01-31 14:10:33 +0100

    Delete LeetCode directory

--- LeetCode/02543 Check if Point Is Reachable/Solution.kt
+++
@@ -1,6 +0,0 @@
-class Solution {
-    fun isReachable(targetX: Int, targetY: Int): Boolean {
-        tailrec fun gcd(x: Int, y: Int): Int = if (x == 0 || y == 0) x + y else gcd(y % x, x)
-        return gcd(targetX, targetY).let { it and it - 1 } == 0
-    }
-}
