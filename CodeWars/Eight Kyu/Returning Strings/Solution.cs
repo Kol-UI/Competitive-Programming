33a09bb58d1ef3203efbc0a1b6926189df5df02e
Kol <62248045+Kol-UI@users.noreply.github.com>
2023-05-13 10:38:44 +0200

    Commit Returning Strings

---
+++ CodeWars/Eight Kyu/Returning Strings/Solution.cs
@@ -0,0 +1,13 @@
+// Returning Strings
+
+using System;
+namespace CompetitiveProgramming.CodeWars.EightKyu.ReturningStrings
+{
+    public static class Solution
+    {
+        public static string Greet(string name)
+        {
+            return "Hello, " + name + " how are you doing today?";
+        }
+    }
+}
