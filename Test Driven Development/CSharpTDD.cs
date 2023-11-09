c2eeef35a4f144490d94a17720deabbad7535f6f
Kol-UI <nicolas.aubry@live.fr>
2023-11-09 20:26:30 +0100

    LC 1759

--- Test Driven Development/CSharpTDD.cs
+++ Test Driven Development/CSharpTDD.cs
@@ -96,7 +96,7 @@ namespace CompetitiveProgramming.TestDrivenDevelopment
             DesignAuthenticationManagerFunc, InsertDeleteGetRandomFunc, MyCalendarIFunc, MaximizetheConfusionofanExamFunc, AllNodesDistanceKinBinaryTreeFunc, LongestPalindromebyConcatenatingTwoLetterWordsFunc, PathWithMinimumEffortFunc, WhereWilltheBallFallFunc, MinimumASCIIDeleteSumforTwoStringsFunc, CombinationSumIIFunc,
             GroupthePeopleGiventheGroupSizeTheyBelongToFunc, MinimumDeletionstoMakeCharacterFrequenciesUniqueFunc, MinCosttoConnectAllPointsFunc, MinimumOperationstoReduceXtoZeroFunc, ChampagneTowerFunc, RemoveDuplicateLettersFunc, DecodedStringatIndexFunc, OneThreeTwoPatternFunc, RemoveColoredPiecesifBothNeighborsaretheSameColorFunc,
             MajorityElementIIFunc, FindLargestValueinEachTreeRowFunc, KthSymbolinGrammarFunc, BinaryTreesWithFactorsFunc, FindTheOriginalArrayofPrefixXorFunc, CountNodesEqualtoAverageofSubtreeFunc, BuildanArrayWithStackOperationsFunc, LastMomentBeforeAllAntsFallOutofaPlankFunc, FindtheWinnerofanArrayGameFunc, EliminateMaximumNumberofMonstersFunc,
-            DetermineifaCellIsReachableataGivenTimeFunc,
+            DetermineifaCellIsReachableataGivenTimeFunc, CountNumberofHomogenousSubstringsFunc,
         };
 
         private static Delegate[] leetCodeHardList = new Delegate[]
@@ -6326,6 +6326,14 @@ namespace CompetitiveProgramming.TestDrivenDevelopment
             Program.Space();
             Program.Title("Determine if a Cell Is Reachable at a Given Time");
             ResultTester.CheckSolution("LC", LeetCode.DetermineifaCellIsReachableataGivenTime.Test.TestCases());
+        }
+
+
+        private static void CountNumberofHomogenousSubstringsFunc()
+        {
+            Program.Space();
+            Program.Title("Count Number of Homogenous Substrings");
+            ResultTester.CheckSolution("LC", LeetCode.CountNumberofHomogenousSubstrings.Test.TestCases());
         }
 
         
