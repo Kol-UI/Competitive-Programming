5111a8953e3edbbdd849d8521ed242d0559bce6d
Kol-UI <nicolas.aubry@live.fr>
2024-01-05 22:56:11 +0100

    LC 2784

---
+++ Solutions/LeetCode/02784 Check if Array is Good/Solution.cs
@@ -0,0 +1,50 @@
+// Check if Array is Good
+
+
+using System;
+using CompetitiveProgramming.TestDrivenDevelopment;
+namespace CompetitiveProgramming.LeetCode.CheckifArrayisGood
+{
+    public class Solution
+    {
+        public static bool IsGood(int[] nums)
+        {
+            Array.Sort(nums);
+
+            for(int i = 0; i < nums.Length; i++)
+            {
+                if(i < nums.Length - 1)
+                {
+                    if(nums[i] != i + 1)
+                    {
+                        return false;
+                    }
+                }
+                else
+                {
+                    if(nums[i] != i)
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            return true;
+        }
+    }
+
+    public class Test
+    {
+        public static bool[] TestCases()
+        {
+            bool[] results = new bool[]
+            {
+                ResultTester.CheckResult<bool>(Solution.IsGood(new int[] {2, 1, 3}), false),
+                ResultTester.CheckResult<bool>(Solution.IsGood(new int[] {1, 3, 3, 2}), true),
+                ResultTester.CheckResult<bool>(Solution.IsGood(new int[] {1, 1}), true),
+                ResultTester.CheckResult<bool>(Solution.IsGood(new int[] {3, 4, 4, 1, 2, 1}), false),
+            };
+            return results;
+        }
+    }
+}
