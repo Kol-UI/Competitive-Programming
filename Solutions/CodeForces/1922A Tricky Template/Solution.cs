7b9d586064f9628f697438cf63cec7b0d0aa0dd0
Kol-UI <nicolas.aubry@live.fr>
2024-02-08 22:20:42 +0100

    Tricky Template

---
+++ Solutions/CodeForces/1922A Tricky Template/Solution.cs
@@ -0,0 +1,46 @@
+// Tricky Template
+
+using System.Runtime.CompilerServices;
+namespace CompetitiveProgramming.CodeForces.TrickyTemplate
+{
+    class Program
+    {
+        /*
+        void YESNO(bool b) => Console.WriteLine(b ? "YES" : "NO");
+ 
+        static void Main(string[] args)
+        {
+            Console.SetIn(new StreamReader(Console.OpenStandardInput(8192)));
+ 
+            var program = new Program();
+ 
+            program.Solve();
+ 
+            Console.Out.Flush();
+            Console.Read();
+        }
+ 
+        public void Solve()
+        {
+            int zzz = GetInt();
+ 
+            for (int zz = 0; zz < zzz; zz++)
+            {
+                Multi(out int n);
+ 
+                string a = GetStr();
+                string b = GetStr();
+                string c = GetStr();
+ 
+                bool flag = false;
+                for (int i = 0; i < n; i++)
+                {
+                    flag |= a[i] != c[i] && b[i] != c[i];
+                }
+ 
+                YESNO(flag);
+            }
+        }
+        */
+    }
+}
