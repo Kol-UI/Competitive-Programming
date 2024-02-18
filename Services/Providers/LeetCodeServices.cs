325072eb5b2bbcad08a8fef084ba632c9a78fcb0
Kol-UI <nicolas.aubry@live.fr>
2024-02-18 13:36:49 +0100

    LC 3038

--- Services/Providers/LeetCodeServices.cs
+++ Services/Providers/LeetCodeServices.cs
@@ -55,7 +55,7 @@ namespace CompetitiveProgramming.Services.Providers
             FindtheMaximumDivisibilityScoreFunc, RowWithMaximumOnesFunc, FindtheWidthofColumnsofaGridFunc, FindtheLongestBalancedSubstringofaBinaryStringFunc, MinimumNumberofPushestoTypeWordIFunc, DivideanArrayIntoSubarraysWithMinimumCostIFunc, FormSmallestNumberFromTwoDigitArraysFunc,
             NumberofEvenandOddBitsFunc, DistributeMoneytoMaximumChildrenFunc, CounttheNumberofVowelStringsinRangeFunc, MaximumLengthofaConcatenatedStringwithUniqueCharactersFunc, LeftandRightSumDifferencesFunc, FindtheArrayConcatenationValueFunc, TakeGiftsFromtheRichestPileFunc,
             SeparatetheDigitsinanArrayFunc, CountDistinctNumbersonBoardFunc, DifferenceBetweenElementSumandDigitSumofanArrayFunc, MaximumCountofPositiveIntegerandNegativeIntegerFunc, CategorizeBoxAccordingtoCriteriaFunc, CounttheDigitsThatDivideaNumberFunc,
-            ShortestDistancetoTargetStringinaCircularArrayFunc, FindFirstPalindromicStringintheArray, MaximumEnemyFortsThatCanBeCapturedFunc, CountPairsOfSimilarStringsFunc, DeleteGreatestValueinEachRowFunc, LongestUnequalAdjacentGroupsSubsequenceIFunc,
+            ShortestDistancetoTargetStringinaCircularArrayFunc, FindFirstPalindromicStringintheArray, MaximumEnemyFortsThatCanBeCapturedFunc, CountPairsOfSimilarStringsFunc, DeleteGreatestValueinEachRowFunc, LongestUnequalAdjacentGroupsSubsequenceIFunc, MaximumNumberofOperationsWiththeSameScoreIFunc,
         };
 
         private static readonly Delegate[] leetCodeMediumList = new Delegate[]
@@ -111,6 +111,13 @@ namespace CompetitiveProgramming.Services.Providers
         };
 
         // Functions 
+
+        private static void MaximumNumberofOperationsWiththeSameScoreIFunc()
+        {
+            StyleHelper.Space();
+            StyleHelper.Title("Maximum Number of Operations With the Same Score I");
+            ResultTester.CheckSolution(ProblemOrigin.LeetCode, LeetCode.MaximumNumberofOperationsWiththeSameScoreI.Test.TestCases());
+        }
 
         private static void MeetingRoomsIIIFunc()
         {
---
+++ Solutions/LeetCode/03038 Maximum Number of Operations With the Same Score I/Solution.cs
@@ -0,0 +1,46 @@
+// Maximum Number of Operations With the Same Score I
+
+
+using CompetitiveProgramming.TestDrivenDevelopment;
+
+namespace CompetitiveProgramming.LeetCode.MaximumNumberofOperationsWiththeSameScoreI
+{
+    public class Solution
+    {
+        public static int MaxOperations(int[] nums)
+        {
+            int sumOfRemovedValues = nums[0] + nums[1];
+
+            int count = 0;
+
+            while (nums.Length >= 2)
+            {
+                if (nums[0] + nums[1] == sumOfRemovedValues)
+                {
+                    nums = nums.Skip(2).ToArray();
+
+                    count++;
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            return count;
+        }
+    }
+
+    public class Test
+    {
+        public static bool[] TestCases()
+        {
+            bool[] results = new bool[]
+            {
+                ResultTester.CheckResult<int>(Solution.MaxOperations(new int[]{3,2,1,4,5}), 2),
+                ResultTester.CheckResult<int>(Solution.MaxOperations(new int[]{3,2,6,1,4}), 1),
+            };
+            return results;
+        }
+    }
+}
