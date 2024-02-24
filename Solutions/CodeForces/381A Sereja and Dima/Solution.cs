9f8192b8f6f1b2167b88366f68d6a2270c0b9af2
Kol-UI <nicolas.aubry@live.fr>
2024-02-24 23:39:58 +0100

    CF 381A

---
+++ Solutions/CodeForces/381A Sereja and Dima/Solution.cs
@@ -0,0 +1,54 @@
+// Sereja and Dima
+
+
+using System;
+using CompetitiveProgramming.TestDrivenDevelopment;
+
+namespace CompetitiveProgramming.CodeForces.SerejaandDima
+{
+    public class Program
+    {
+        //public static void Main()
+        public static void Solution()
+        {
+            int n = int.Parse(Console.ReadLine()!);
+            string[] cards = Console.ReadLine()!.Split(' ');
+
+            int left = 0;
+            int right = n - 1;
+            int sereja = 0;
+            int dima = 0;
+
+            bool serejaTurn = true;
+
+            while (left <= right)
+            {
+                int leftCard = int.Parse(cards[left]);
+                int rightCard = int.Parse(cards[right]);
+
+                if (serejaTurn)
+                {
+                    sereja += Math.Max(leftCard, rightCard);
+                }
+                else
+                {
+                    dima += Math.Max(leftCard, rightCard);
+                }
+
+                if (leftCard > rightCard)
+                {
+                    left++;
+                }
+                else
+                {
+                    right--;
+                }
+
+                serejaTurn = !serejaTurn;
+            }
+
+            Console.WriteLine($"{sereja} {dima}");
+        }
+    }
+
+}
