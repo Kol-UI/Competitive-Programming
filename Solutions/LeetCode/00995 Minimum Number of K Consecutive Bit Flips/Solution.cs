e861e8ec4fd83a9979d44b6b515aabd75bce6f37
Kol-UI <nicolas.aubry@live.fr>
2024-06-24 22:19:38 +0200

    995 LC

---
+++ Solutions/LeetCode/00995 Minimum Number of K Consecutive Bit Flips/Solution.cs
@@ -0,0 +1,46 @@
+// Minimum Number of K Consecutive Bit Flips
+
+using CompetitiveProgramming.Helpers;
+using CompetitiveProgramming.Models;
+using CompetitiveProgramming.TestDrivenDevelopment;
+
+namespace CompetitiveProgramming.LeetCode.MinimumNumberofKConsecutiveBitFlips
+{
+    public class Solution
+    {
+        public int MinKBitFlips(int[] nums, int k)
+        {
+            var count = 0;
+            int flip = 0;
+            if(k > nums.Length){
+                return -1;
+            }
+            for(var i = 0;i< nums.Length;i++){
+                if(i >= k){
+                    flip ^= 1 - nums[i - k];
+                }
+                if(nums[i] == flip){
+                    if(i + k > nums.Length){
+                        return -1;
+                    }
+                    nums[i] = 0;
+                    flip ^= 1;
+                    count++;
+                } else {
+                    nums[i] = 1;
+                }
+            }
+            return count;
+        }
+    }
+
+    public class TestSolution : BaseSolution
+    {
+        public override void GetResult()
+        {
+            StyleHelper.Space();
+            StyleHelper.Title("Minimum Number of K Consecutive Bit Flips");
+            ResultTester.SpecialTestCase(ProblemOrigin.LeetCode, ProblemCategory.HardLC);
+        }
+    }
+}
