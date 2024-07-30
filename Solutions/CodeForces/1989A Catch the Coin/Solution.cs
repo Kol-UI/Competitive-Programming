fc3f478cf7317ea94c77fc2c9c572c6077e25d15
Kol <62248045+Kol-UI@users.noreply.github.com>
2024-07-30 15:11:16 +0200

    Catch the Coin Codeforces

---
+++ Solutions/CodeForces/1989A Catch the Coin/Solution.cs
@@ -0,0 +1,34 @@
+
+using CompetitiveProgramming.Helpers;
+using CompetitiveProgramming.Models;
+using CompetitiveProgramming.TestDrivenDevelopment;
+
+namespace CompetitiveProgramming.CodeForces.CatchtheCoin
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            int testCases = int.Parse(Console.ReadLine()!);
+
+            for (int i = 0; i < testCases; i++)
+            {
+                string[] input = Console.ReadLine()!.Split();
+                long x = long.Parse(input[0]);
+                long y = long.Parse(input[1]);
+
+                Console.WriteLine(y >= -1 ? "YES" : "NO");
+            }
+        }
+    }
+
+    public class TestSolution : BaseSolution
+    {
+        public override void GetResult()
+        {
+            StyleHelper.Space();
+            StyleHelper.Title("Catch the Coin");
+            ResultTester.SpecialTestCase(ProblemOrigin.CodeForces, ProblemCategory.CF800);
+        }
+    }
+}
