a1e27518ed7b330195cc8899650a486dceb0d679
Kol-UI <nicolas.aubry@live.fr>
2024-08-07 18:27:31 +0200

    LC 1050

---
+++ Solutions/LeetCode/01050 Actors and Directors Who Cooperated At Least Three Times/Solution.cs
@@ -0,0 +1,19 @@
+// Actors and Directors Who Cooperated At Least Three Times
+
+using CompetitiveProgramming.Helpers;
+using CompetitiveProgramming.Models;
+using CompetitiveProgramming.TestDrivenDevelopment;
+
+namespace CompetitiveProgramming.LeetCode.ActorsandDirectorsWhoCooperatedAtLeastThreeTimes
+{
+    public class TestSolution : BaseSolution
+    {
+        public override void GetResult()
+        {
+            StyleHelper.Space();
+            StyleHelper.Title("Actors and Directors Who Cooperated At Least Three Times");
+            ResultTester.SpecialTestCase(ProblemOrigin.LeetCode, ProblemCategory.EasyLC);
+            ResultTester.SpecialTestCase(ProblemOrigin.LeetCode, ProblemCategory.MediumLC);
+        }
+    }
+}
