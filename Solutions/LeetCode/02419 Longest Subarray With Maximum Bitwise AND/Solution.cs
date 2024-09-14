1262b3ea620269d555234b54780d1fc652a60d6c
Kol-UI <nicolas.aubry@live.fr>
2024-09-14 21:25:20 +0200

    LC 2419

---
+++ Solutions/LeetCode/02419 Longest Subarray With Maximum Bitwise AND/Solution.cs
@@ -0,0 +1,43 @@
+// Longest Subarray With Maximum Bitwise AND
+
+using CompetitiveProgramming.Helpers;
+using CompetitiveProgramming.Models;
+using CompetitiveProgramming.TestDrivenDevelopment;
+
+namespace CompetitiveProgramming.LeetCode.LongestSubarrayWithMaximumBitwiseAND
+{
+    public class Solution
+    {
+        public int LongestSubarray(int[] nums) {
+            int maxVal = 0;
+            
+            foreach (int num in nums) {
+                maxVal = Math.Max(maxVal, num);
+            }
+            
+            int maxLength = 0;
+            int currentLength = 0;
+            
+            foreach (int num in nums) {
+                if (num == maxVal) {
+                    currentLength++;
+                    maxLength = Math.Max(maxLength, currentLength);
+                } else {
+                    currentLength = 0;
+                }
+            }
+            
+            return maxLength;
+        }
+    }
+
+    public class TestSolution : BaseSolution
+    {
+        public override void GetResult()
+        {
+            StyleHelper.Space();
+            StyleHelper.Title("Longest Subarray With Maximum Bitwise AND");
+            ResultTester.SpecialTestCase(ProblemOrigin.LeetCode, ProblemCategory.EasyLC);
+        }
+    }
+}
