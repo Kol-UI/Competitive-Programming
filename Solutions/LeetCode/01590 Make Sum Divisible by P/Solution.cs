e2666f8854fd134302f7ad96094a5f834878f24b
Kol-UI <nicolas.aubry@live.fr>
2024-10-03 20:25:15 +0200

    LC 1590

---
+++ Solutions/LeetCode/01590 Make Sum Divisible by P/Solution.cs
@@ -0,0 +1,60 @@
+// Make Sum Divisible by P
+
+using CompetitiveProgramming.Helpers;
+using CompetitiveProgramming.Models;
+using CompetitiveProgramming.TestDrivenDevelopment;
+
+namespace CompetitiveProgramming.LeetCode.MakeSumDivisiblebyP
+{
+    public class Solution
+    {
+        public int MinSubarray(int[] nums, int p) {
+            int n = nums.Length;
+            long totalSum = 0;
+            
+            foreach (int num in nums) {
+                totalSum += num;
+            }
+
+            long remainder = totalSum % p;
+            if (remainder == 0) {
+                return 0;
+            }
+
+            Dictionary<long, int> prefixMap = new Dictionary<long, int>();
+            prefixMap[0] = -1;  
+
+            long prefixSum = 0;
+            int minLength = n;
+
+            for (int i = 0; i < n; i++) {
+
+                prefixSum += nums[i];
+
+                long currentMod = prefixSum % p;
+
+
+                long targetMod = (currentMod - remainder + p) % p;
+
+
+                if (prefixMap.ContainsKey(targetMod)) {
+                    minLength = Math.Min(minLength, i - prefixMap[targetMod]);
+                }
+
+                prefixMap[currentMod] = i;
+            }
+
+            return minLength == n ? -1 : minLength;
+        }
+    }
+
+    public class TestSolution : BaseSolution
+    {
+        public override void GetResult()
+        {
+            StyleHelper.Space();
+            StyleHelper.Title("Make Sum Divisible by P");
+            ResultTester.SpecialTestCase(ProblemOrigin.LeetCode, ProblemCategory.MediumLC);
+        }
+    }
+}
