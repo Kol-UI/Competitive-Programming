29ab6c0b3fa073aa928b76c97af2baa89a570043
Kol-UI <nicolas.aubry@live.fr>
2024-10-11 22:00:35 +0200

    LC 1942

---
+++ Solutions/LeetCode/01942 The Number of the Smallest Unoccupied Chair/Solution.cs
@@ -0,0 +1,63 @@
+// The Number of the Smallest Unoccupied Chair
+
+using CompetitiveProgramming.Helpers;
+using CompetitiveProgramming.Models;
+using CompetitiveProgramming.TestDrivenDevelopment;
+
+namespace CompetitiveProgramming.LeetCode.TheNumberoftheSmallestUnoccupiedChair
+{
+    public class Solution
+    {
+        public int SmallestChair(int[][] times, int targetFriend) {
+            int n = times.Length;
+
+            int[][] timesWithIndex = new int[n][];
+            for (int i = 0; i < n; i++) {
+                timesWithIndex[i] = new int[] { times[i][0], times[i][1], i };
+            }
+
+            Array.Sort(timesWithIndex, (a, b) => a[0].CompareTo(b[0]));
+
+            bool[] occupied = new bool[n];
+            int[] chairAssignment = new int[n]; 
+            var leavingList = new List<Tuple<int, int>>();  
+            
+            for (int i = 0; i < n; i++) {
+                int arrivalTime = timesWithIndex[i][0];
+                int leavingTime = timesWithIndex[i][1];
+                int friendIndex = timesWithIndex[i][2];
+                leavingList.RemoveAll(l => {
+                    if (l.Item1 <= arrivalTime) {
+                        occupied[l.Item2] = false;  
+                        return true;
+                    }
+                    return false;
+                });
+                int assignedChair = -1;
+                for (int j = 0; j < n; j++) {
+                    if (!occupied[j]) {
+                        assignedChair = j;
+                        occupied[j] = true;  
+                        break;
+                    }
+                }
+                chairAssignment[friendIndex] = assignedChair;
+                if (friendIndex == targetFriend) {
+                    return assignedChair;
+                }
+                leavingList.Add(Tuple.Create(leavingTime, assignedChair));
+            }
+            return -1;
+        }
+    }
+
+    public class TestSolution : BaseSolution
+    {
+        public override void GetResult()
+        {
+            StyleHelper.Space();
+            StyleHelper.Title("The Number of the Smallest Unoccupied Chair");
+            ResultTester.SpecialTestCase(ProblemOrigin.LeetCode, ProblemCategory.MediumLC);
+        }
+    }
+}
