cde52ed3a9338b16326882afe259934c16707169
Kol-UI <nicolas.aubry@live.fr>
2024-10-12 18:04:04 +0200

    CF 1899A

---
+++ Solutions/CodeForces/1899A Game with Integers/Solution.cs
@@ -0,0 +1,54 @@
+// Game with Integers
+
+using CompetitiveProgramming.Helpers;
+using CompetitiveProgramming.Models;
+using CompetitiveProgramming.TestDrivenDevelopment;
+
+namespace CompetitiveProgramming.CodeForces.GamewithIntegers
+{
+    public class Solution
+    {
+        //public static void Main(string[] args)
+        public static void MainSolution()
+        {
+            int numberOfTestCases = ReadInt();
+
+            List<string> results = new List<string>();
+
+            for (int i = 0; i < numberOfTestCases; i++)
+            {
+                int n = ReadInt();
+                if (n % 3 != 0)
+                {
+                    results.Add("First");
+                }
+                else
+                {
+                    results.Add("Second");
+                }
+            }
+
+            PrintResults(results);
+        }
+
+        private static string ReadLn() => Console.ReadLine()!;
+        private static int ReadInt() => int.Parse(ReadLn());
+        private static long ReadLong() => long.Parse(ReadLn());
+        private static double ReadDouble() => double.Parse(ReadLn());
+        private static string[] ReadStrings() => ReadLn().Split(' ');
+        private static int[] ReadInts() => ReadStrings().Select(int.Parse).ToArray();
+        private static long[] ReadLongs() => ReadStrings().Select(long.Parse).ToArray();
+        private static double[] ReadDoubles() => ReadStrings().Select(double.Parse).ToArray();
+        private static void PrintResults<T>(List<T> results) => results.ForEach(result => Console.WriteLine(result?.ToString()));
+    }
+
+    public class TestSolution : BaseSolution
+    {
+        public override void GetResult()
+        {
+            StyleHelper.Space();
+            StyleHelper.Title("Game with Integers");
+            ResultTester.SpecialTestCase(ProblemOrigin.CodeForces, ProblemCategory.CF800);
+        }
+    }
+}
