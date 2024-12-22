0a638cd85a14326bf0577ed7ba41a12c1452e4c6
Kol-UI <nicolas.aubry@live.fr>
2024-12-22 13:52:37 +0100

    LC 2940

---
+++ Solutions/LeetCode/02940 Find Building Where Alice and Bob Can Meet/Solution.cs
@@ -0,0 +1,18 @@
+// Find Building Where Alice and Bob Can Meet
+
+using CompetitiveProgramming.Helpers;
+using CompetitiveProgramming.Models;
+using CompetitiveProgramming.TestDrivenDevelopment;
+
+namespace CompetitiveProgramming.LeetCode.FindBuildingWhereAliceandBobCanMeet
+{
+    public class TestSolution : BaseSolution
+    {
+        public override void GetResult()
+        {
+            StyleHelper.Space();
+            StyleHelper.Title("Find Building Where Alice and Bob Can Meet");
+            ResultTester.SpecialTestCase(ProblemOrigin.LeetCode, ProblemCategory.HardLC);
+        }
+    }
+}
