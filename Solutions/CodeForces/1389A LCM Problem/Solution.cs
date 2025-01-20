ea8429dc0db3f1ab5358a83bda8236eab7f85f52
Kol-UI <nicolas.aubry@live.fr>
2025-01-20 19:58:25 +0100

    CF 1389A

---
+++ Solutions/CodeForces/1389A LCM Problem/Solution.cs
@@ -0,0 +1,55 @@
+// LCM Problem
+
+namespace CompetitiveProgramming.CodeForces.LCMProblem;
+using CompetitiveProgramming.Helpers;
+using CompetitiveProgramming.Models;
+using CompetitiveProgramming.TestDrivenDevelopment;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Solution
+{
+    //public static void Main(string[] args)
+    public static void MainSolution()
+    {
+        int numberOfTestCases = ReadInt();
+        List<string> results = new List<string>();
+
+        for (int i = 0; i < numberOfTestCases; i++)
+        {
+            int[] lr = ReadInts();
+            int l = lr[0], r = lr[1];
+
+            results.Add(Solve(l, r));
+        }
+
+        PrintResults(results);
+    }
+
+    static string Solve(int l, int r)
+    {
+        return (l * 2 <= r) ? $"{l} {l * 2}" : "-1 -1";
+    }
+
+    private static string ReadLn() => Console.ReadLine()!;
+    private static int ReadInt() => int.Parse(ReadLn());
+    private static long ReadLong() => long.Parse(ReadLn());
+    private static double ReadDouble() => double.Parse(ReadLn());
+    private static string[] ReadStrings() => ReadLn().Split(' ');
+    private static int[] ReadInts() => ReadStrings().Select(int.Parse).ToArray();
+    private static long[] ReadLongs() => ReadStrings().Select(long.Parse).ToArray();
+    private static double[] ReadDoubles() => ReadStrings().Select(double.Parse).ToArray();
+    private static void PrintResults<T>(List<T> results) => results.ForEach(result => Console.WriteLine(result?.ToString()));
+}
+
+public class TestSolution : BaseSolution
+{
+    public override void GetResult()
+    {
+        StyleHelper.Space();
+        StyleHelper.Title("LCM Problem");
+        ResultTester.SpecialTestCase(ProblemOrigin.CodeForces, ProblemCategory.CF800);
+    }
+}
