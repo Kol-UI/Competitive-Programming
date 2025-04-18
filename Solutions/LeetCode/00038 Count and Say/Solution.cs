a6e8aa27769170abdfd30ecb5c5122f0ef504f3a
Kol-UI <nicolas.aubry@live.fr>
2025-04-18 20:55:41 +0200

    LC 38

---
+++ Solutions/LeetCode/00038 Count and Say/Solution.cs
@@ -0,0 +1,46 @@
+// Count and Say
+namespace CompetitiveProgramming.LeetCode.CountandSay;
+
+using System.Text;
+using CompetitiveProgramming.Helpers;
+using CompetitiveProgramming.Models;
+using CompetitiveProgramming.TestDrivenDevelopment;
+
+public class Solution
+{
+    public string CountAndSay(int n)
+    {
+        return Helper(n, "1");
+    }
+
+    private string Helper(int n, string str)
+    {
+        if(n <= 1) return str;
+        StringBuilder sb = new();
+        
+        for(int i = 0; i < str.Length; i++)
+        {
+            int count = 1;
+            char digit = str[i];
+
+            while(i + 1 < str.Length && str[i] == str[i + 1])
+            {
+                count++;
+                i++;
+            }
+
+            sb.Append(count).Append(digit);
+        }
+        return Helper(n - 1, sb.ToString());
+    }
+}
+
+public class TestSolution : BaseSolution
+{
+    public override void GetResult()
+    {
+        StyleHelper.Space();
+        StyleHelper.Title("Count and Say");
+        ResultTester.SpecialTestCase(ProblemOrigin.LeetCode, ProblemCategory.MediumLC);
+    }
+}
