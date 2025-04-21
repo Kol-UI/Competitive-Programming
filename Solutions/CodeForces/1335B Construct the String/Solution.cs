a19ff2d62cdcdaceec04212220122eacfb20ab65
Kol-UI <nicolas.aubry@live.fr>
2025-04-21 17:12:53 +0200

    CF 1335B

---
+++ Solutions/CodeForces/1335B Construct the String/Solution.cs
@@ -0,0 +1,45 @@
+// Construct the String
+namespace CompetitiveProgramming.CodeForces.ConstructtheString;
+using CompetitiveProgramming.Helpers;
+using CompetitiveProgramming.Models;
+using CompetitiveProgramming.TestDrivenDevelopment;
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        int t = int.Parse(Console.ReadLine()!);
+        while (t-- > 0)
+        {
+            string[] input = Console.ReadLine()!.Split();
+            int n = int.Parse(input[0]);
+            int a = int.Parse(input[1]);
+            int b = int.Parse(input[2]);
+            char c = 'a';
+            int cnt = 0;
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write(c++);
+                cnt++;
+                if (cnt == b)
+                {
+                    c = 'a';
+                    cnt = 0;
+                }
+            }
+            Console.WriteLine();
+        }
+    }
+}
+
+public class TestSolution : BaseSolution
+{
+    public override void GetResult()
+    {
+        StyleHelper.Space();
+        StyleHelper.Title("Construct the String");
+        ResultTester.SpecialTestCase(ProblemOrigin.CodeForces, ProblemCategory.CF900);
+    }
+}
