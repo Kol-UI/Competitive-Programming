df4c7d793102ce87012e2fe51604be69c6f7d9b4
Kol-UI <nicolas.aubry@live.fr>
2025-05-01 20:22:10 +0200

    CF 1992C

---
+++ Solutions/CodeForces/1992C Gorilla and Permutation/Solution.cs
@@ -0,0 +1,47 @@
+// Gorilla and Permutation
+namespace CompetitiveProgramming.CodeForces.GorillaandPermutation;
+using CompetitiveProgramming.Helpers;
+using CompetitiveProgramming.Models;
+using CompetitiveProgramming.TestDrivenDevelopment;
+
+using System;
+
+class Program
+{
+    //static void Main()
+    public static void MainSolution()
+    {
+        int tc = int.Parse(Console.ReadLine()!);
+        while (tc-- > 0)
+        {
+            string[] input = Console.ReadLine()!.Split();
+            int n = int.Parse(input[0]);
+            int m = int.Parse(input[1]);
+            int k = int.Parse(input[2]);
+            int cur = n;
+            
+            while (cur > m)
+            {
+                Console.Write(cur + " ");
+                cur--;
+            }
+            
+            for (int i = 1; i <= m; i++)
+            {
+                Console.Write(i + " ");
+            }
+            
+            Console.WriteLine();
+        }
+    }
+}
+
+public class TestSolution : BaseSolution
+{
+    public override void GetResult()
+    {
+        StyleHelper.Space();
+        StyleHelper.Title("orilla and Permutation");
+        ResultTester.SpecialTestCase(ProblemOrigin.CodeForces, ProblemCategory.CF900);
+    }
+}
