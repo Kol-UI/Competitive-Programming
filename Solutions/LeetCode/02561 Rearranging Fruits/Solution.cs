8ad07f60a490e3c5b16a938e9cb3cecfebb10a34
Kol-UI <nicolas.aubry@live.fr>
2025-08-02 23:16:32 +0200

    LC 2561

---
+++ Solutions/LeetCode/02561 Rearranging Fruits/Solution.cs
@@ -0,0 +1,52 @@
+// Rearranging Fruits
+namespace CompetitiveProgramming.LeetCode.RearrangingFruits;
+using CompetitiveProgramming.Helpers;
+using CompetitiveProgramming.Models;
+using CompetitiveProgramming.TestDrivenDevelopment;
+
+public class Solution {
+    public long MinCost(int[] basket1, int[] basket2) {
+        var freq = new Dictionary<int, int>();
+        int m = int.MaxValue;
+
+        foreach (var b in basket1) {
+            if (!freq.ContainsKey(b))
+                freq[b] = 0;
+            freq[b]++;
+            m = Math.Min(m, b);
+        }
+        foreach (var b in basket2) {
+            if (!freq.ContainsKey(b))
+                freq[b] = 0;
+            freq[b]--;
+            m = Math.Min(m, b);
+        }
+
+        var merge = new List<int>();
+        foreach (var kv in freq) {
+            int c = kv.Value;
+            if (c % 2 != 0)
+                return -1;
+            for (int i = 0; i < Math.Abs(c) / 2; i++) {
+                merge.Add(kv.Key);
+            }
+        }
+
+        merge.Sort();
+        long res = 0;
+        for (int i = 0; i < merge.Count / 2; i++) {
+            res += Math.Min(2 * m, merge[i]);
+        }
+        return res;
+    }
+}
+
+public class TestSolution : BaseSolution
+{
+    public override void GetResult()
+    {
+        StyleHelper.Space();
+        StyleHelper.Title("Rearranging Fruits");
+        ResultTester.SpecialTestCase(ProblemOrigin.LeetCode, ProblemCategory.HardLC);
+    }
+}
