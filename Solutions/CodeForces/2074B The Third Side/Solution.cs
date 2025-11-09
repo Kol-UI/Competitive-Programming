587dd93cf7693fb106909db78baa1d3369670269
Kol-UI <nicolas.aubry@live.fr>
2025-11-09 23:03:06 +0100

    CF 2074B

---
+++ Solutions/CodeForces/2074B The Third Side/Solution.cs
@@ -0,0 +1,38 @@
+// The Third Side
+namespace CompetitiveProgramming.CodeForces.TheThirdSide;
+using CompetitiveProgramming.Helpers;
+using CompetitiveProgramming.Models;
+using CompetitiveProgramming.TestDrivenDevelopment;
+#pragma warning disable CS8602
+#pragma warning disable CS8604
+
+using System;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        int testCases = int.Parse(Console.ReadLine());
+        
+        for (int testCase = 0; testCase < testCases; testCase++)
+        {
+            int n = int.Parse(Console.ReadLine());
+            long[] numbers = Array.ConvertAll(Console.ReadLine().Split(), long.Parse);
+            long sum = numbers.Sum();
+            Console.WriteLine(sum - (n - 1));
+        }
+    }
+}
+
+#pragma warning restore CS8604
+#pragma warning restore CS8602
+public class TestSolution : BaseSolution
+{
+    public override void GetResult()
+    {
+        StyleHelper.Space();
+        StyleHelper.Title("The Third Side");
+        ResultTester.SpecialTestCase(ProblemOrigin.CodeForces, ProblemCategory.CF800);
+    }
+}
