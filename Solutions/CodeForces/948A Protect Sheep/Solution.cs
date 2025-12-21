957814cedf33a8ba32f95b24cec487a8948e123a
Kol-UI <nicolas.aubry@live.fr>
2025-12-21 22:16:56 +0100

    CF 948A

---
+++ Solutions/CodeForces/948A Protect Sheep/Solution.cs
@@ -0,0 +1,20 @@
+// Protect Sheep
+namespace CompetitiveProgramming.CodeForces.ProtectSheep;
+using CompetitiveProgramming.Helpers;
+using CompetitiveProgramming.Models;
+using CompetitiveProgramming.TestDrivenDevelopment;
+#pragma warning disable CS8602
+#pragma warning disable CS8604
+
+
+#pragma warning restore CS8604
+#pragma warning restore CS8602
+public class TestSolution : BaseSolution
+{
+    public override void GetResult()
+    {
+        StyleHelper.Space();
+        StyleHelper.Title("Protect Sheep");
+        ResultTester.SpecialTestCase(ProblemOrigin.CodeForces, ProblemCategory.CF900);
+    }
+}
