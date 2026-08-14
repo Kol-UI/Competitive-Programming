3f58b80cbfc519a6d737031ef426b28bd429a4c9
Kol-UI <nicolas.aubry@live.fr>
2026-08-14 22:48:13 +0200

    add class photo solution

---
+++ Solutions/Kattis/Class Photo/Solution.cs
@@ -0,0 +1,29 @@
+// Class Photo
+namespace CompetitiveProgramming.Kattis.ClassPhoto;
+using CompetitiveProgramming.Helpers;
+using CompetitiveProgramming.Models;
+using CompetitiveProgramming.TestDrivenDevelopment;
+#pragma warning disable CS8604
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        int a = int.Parse(Console.ReadLine());
+        int b = int.Parse(Console.ReadLine());
+        Console.WriteLine(a * b);
+    }
+}
+
+#pragma warning restore CS8604
+public class TestSolution : BaseSolution
+{
+    public override void GetResult()
+    {
+        StyleHelper.Space();
+        StyleHelper.Title("Class Photo");
+        ResultTester.SpecialTestCase(ProblemOrigin.Kattis, ProblemCategory.EasyKAT);
+    }
+}
